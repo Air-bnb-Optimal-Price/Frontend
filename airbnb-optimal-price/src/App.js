a17fa3101cb04418efdc68442f1ac9e2e6f09c83
Gideon <reachgiddy@gmail.com>
2020-02-03 13:08:22 -0500

    Created app skeleton & background image to App.js

---
+++ airbnb-optimal-price/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import RegisterLogin from './components/RegisterLoginSection/RegisterLoginContainer/RegisterLogin';
+
+function App() {
+  return (
+    <div className='App'>
+      <RegisterLogin />
+    </div>
+  );
+}
+
+export default App;
